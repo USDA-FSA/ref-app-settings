e17d278f9df2d08eccab6f2ed3d04d6857980b2d
Francis Rupert <francisrupert@gmail.com>
2018-07-26 14:12:57 -0500

    Sample validation of form fields [#2]

--- src/js/demo/form-field.js
+++ src/js/demo/form-field.js
@@ -18,7 +18,45 @@ $('body').on('change', '[data-behavior~="track-change"]', function(event) {
 
   var $self = $(this);
   var $root = $self.closest('.fsa-modal');
+  var $saver = $root.find('[data-saver]');
 
   $root.attr('data-changed', 'yup');
+  $saver.removeAttr('disabled');
+
+})
+
+$('body').on('blur', '[data-behavior~="all-filled"]', function(event) {
+
+  console.log('Checking inputs');
+
+  var $self = $(this);
+  var $root = $self.closest('.fsa-modal');
+  var $inputs = $root.find('.fsa-field__item');
+  var $saver = $root.find('[data-saver]');
+
+  var inputsComplete = true;
+
+  $inputs.each(function() {
+
+    console.log('input item');
+
+    // If the input is not valid
+    if (!$(this).val()){
+      // Set variable to not valid
+      inputsComplete = false;
+      console.log('inputsComplete = false');
+    }
+
+    // If all variables are valid
+    if (inputsComplete == true) {
+      $saver.removeAttr('disabled');
+      console.log('inputsComplete == true');
+    }
+    else {
+      $saver.attr('disabled', 'disabled');
+      console.log('elsed');
+    }
+
+  });
 
 })
