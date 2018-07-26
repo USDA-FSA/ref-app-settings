c108b1f1e16a62aa243938680727adf291697fd7
Francis Rupert <francisrupert@gmail.com>
2018-07-26 07:34:52 -0500

    Hint Toggle [amends #2]

--- src/js/ref-app-settings.js
+++ src/js/ref-app-settings.js
@@ -11,13 +11,15 @@
 
 'use strict';
 
+// From FSA Design System
 var NavGlobalComponent      = require('./components/fsa-nav-global');
 var ModalComponent          = require('./components/fsa-modal');
 var GrowlComponent          = require('./components/fsa-growl');
-var GrowlComponentExtended  = require('./components/fsa-growl--extended.js');
 var SelectMultipleComponent = require('./components/fsa-select-multi');
 var SteppedControlComponent = require('./components/fsa-stepped-control');
 var SpinboxComponent        = require('./components/fsa-spinbox');
 
+// Project-specifici
+var GrowlComponentExtended  = require('./components/fsa-growl--extended.js');
 var HintToggle              = require('./demo/hint-toggle');
 var FormField               = require('./demo/form-field');
